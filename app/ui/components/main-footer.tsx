c11a26120262359016fd86c51c93e151eae50a85
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2026-01-29 22:02:00 +0200

    Project Setup

---
+++ app/ui/components/main-footer.tsx
@@ -0,0 +1,15 @@
+export default function MainFooter() {
+    return (
+        <div className="bg-gray-900 text-white px-15 py-10 flex justify-between">
+            <div>Site name</div>
+            <nav className="flex gap-5">
+                <ul className="flex gap-10">
+                    <li>Page</li>
+                    <li>Page</li>
+                    <li>Page</li>
+                </ul>
+                <button>Button</button>
+            </nav>
+        </div>
+    )
+}
