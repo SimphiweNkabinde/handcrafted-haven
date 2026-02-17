1332ee739a77ea8f2802d58fb5d4ae95994263a8
bmcaldarella <bmcaldarella@gmail.com>
2026-02-17 22:24:54 +0000

    My Profile

--- app/ui/components/main-header.tsx
+++ app/ui/components/main-header.tsx
@@ -5,7 +5,7 @@ import MobileMenu from "@/app/ui/mobile-menu";
 
 const navItems = [
   { href: "/products", label: "Browse" },
-  { href: "/profile", label: "Sell" },
+  { href: "/profile", label: "My Profile" },
   { href: "/profiles", label: "Discover" },
 ];
 
